b512d1254cade797a57c8a0fcd6be1715bdb15cc
Tawanda <ejmunongo@gmail.com>
2025-12-11 15:58:05 +0200

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/tmunongo/nanotools/internal/handlers"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	fileServer := http.FileServer(http.Dir("web/static"))
+	r.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	r.Get("/", handlers.HomeHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	addr := fmt.Sprintf(":%s", port)
+	fmt.Printf("Starting TinyUtils on %s\n", addr)
+	
+	if err := http.ListenAndServe(addr, r); err != nil {
+		fmt.Fprintf(os.Stderr, "Server failed to start: %v\n", err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tmunongo/nanotools
+
+go 1.25.4
+
+require (
+	github.com/a-h/templ v0.3.960 // indirect
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+)
